e39f8e942945b4c6f83acd0b1129a8f6b43dce2b
Jaganathg <30722985+jaganathg@users.noreply.github.com>
2024-06-24 11:08:34 +0200

    add 577 easy sql

---
+++ Easy/577_Employee_Bonus.sql
@@ -0,0 +1,21 @@
+Create table If Not Exists Employee (empId int, name varchar(255), supervisor int, salary int);
+Create table If Not Exists Bonus (empId int, bonus int);
+Truncate table Employee;
+insert into Employee (empId, name, supervisor, salary) values ('3', 'Brad', NULL, '4000');
+insert into Employee (empId, name, supervisor, salary) values ('1', 'John', '3', '1000');
+insert into Employee (empId, name, supervisor, salary) values ('2', 'Dan', '3', '2000');
+insert into Employee (empId, name, supervisor, salary) values ('4', 'Thomas', '3', '4000');
+Truncate table Bonus;
+insert into Bonus (empId, bonus) values ('2', '500');
+insert into Bonus (empId, bonus) values ('4', '2000');
+
+
+select * from employee;
+select * from bonus;
+
+
+select e.name, b.bonus
+from employee as e
+left join bonus as b
+	on e.empid = b.empid
+where b.bonus is null or b.bonus < 1000;
